689f4b1f433157bd7a06129c0c2b6eb0b88decc6
srosenberg70 <50153532+srosenberg70@users.noreply.github.com>
2023-05-07 20:36:12 -0400

    Add files via upload

---
+++ internet_gateway.tf
@@ -0,0 +1,28 @@
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "IGW"-"${var.environment}"
+ }
+}
+
+resource "aws_route_table" "table" {
+  vpc_id = "${aws_vpc.main.id}"
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+  tags = {
+    Name = "RT"-"${var.environment}"
+  }
+}
+
+resource "aws_route_table_association" "public_us_east_1a" {
+  subnet_id      = aws_subnet.public_us_east_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_us_east_1b" {
+  subnet_id      = aws_subnet.public_us_east_1b.id
+  route_table_id = aws_route_table.public.id
+}
